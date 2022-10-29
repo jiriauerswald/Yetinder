b7c12f0b5000353a462adbc8864562b7b91c822e
jiriauerswald <107207515+jiriauerswald@users.noreply.github.com>
2022-10-29 21:06:51 +0200

    Best of added

---
+++ src/Controller/YetinderControler.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Controller;
+
+use App;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class YetinderControler extends AbstractController
+{
+    
+    # [Route('/')]
+    /**
+     * @Route("/", name="action")
+     */
+    
+    public function bestOf(): Response
+    {
+        $yetinder = [
+            'Bigfoot' => [
+                'score' => 7,
+                'sex' => 'male',
+                'age' => 32,
+                'height' => 248,
+                'interest' => 'female'
+            ],
+            'Yetigirl' => [
+                'score' => 12,
+                'sex' => 'female',
+                'age' => 21,
+                'height' => 211,
+                'interest' => 'male'
+            ],
+            'Homer' => [
+                'score' => 21,
+                'sex' => 'male',
+                'age' => 32,
+                'height' => 261,
+                'interest' => 'female'
+            ],
+            'Lisa' => [
+                'score' => 12,
+                'sex' => 'female',
+                'age' => 21,
+                'height' => 211,
+                'interest' => 'male'
+            ]
+        ];
+        return $this->render('best_of.html.twig', [
+            'yetinder' => $yetinder
+        ]);
+    }
+    
+    
+    public static function getSubscribedServices(): array
+    {
+        return parent::getSubscribedServices();
+    }
+
+    
+}
